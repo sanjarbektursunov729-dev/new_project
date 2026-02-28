4b4a59e959e72c54b290718d19c7f9947fb11524
adiloveldor1283-cyber <adiloveldor1283@gmai.com>
2026-02-28 15:45:21 +0500

    Update domain settings

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-secret-change-me")
 DEBUG = os.getenv("DJANGO_DEBUG", "0") == "1"
 CSRF_TRUSTED_ORIGINS = [
     "https://web-production-4ac2.up.railway.app",
+    "https://project.upcode.uz",
 ]
 ALLOWED_HOSTS = [h.strip() for h in os.getenv("DJANGO_ALLOWED_HOSTS", "project.upcode.uz,web-production-4ac2.up.railway.app").split(",") if h.strip()]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
